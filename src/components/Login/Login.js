f5454786468407cbad0085b2af919068c47a3186
antor10655gh <creativefreelancerantor@gmail.com>
2022-04-10 04:31:19 +0600

    add onSubmit handle

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,12 +15,12 @@ const Login = () => {
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" required />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" required />
               </Form.Group>
               <Button className="login-btn" type="submit">
                 Login
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Your Password Not Match");
+      return;
+    }
+  };
+
   return (
     <div className="container">
       <div className="row w-50 mx-auto text-start mt-4">
@@ -12,21 +37,37 @@ const SignUp = () => {
             <header className="text-center mb-5">
               <h2>Please Sign Up</h2>
             </header>
-            <Form>
+            <Form onSubmit={handleCreateUser}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                  onBlur={handleEmail}
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  onBlur={handlePassword}
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm Password" />
+                <Form.Control
+                  onBlur={handleConfirmPassword}
+                  type="password"
+                  placeholder="Confirm Password"
+                  required
+                />
               </Form.Group>
+              <p style={{ color: "red" }}>{error}</p>
               <Button className="login-btn" type="submit">
                 Sign Up
               </Button>
